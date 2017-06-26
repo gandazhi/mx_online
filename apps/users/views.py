d0418ee1a88c9a9f16246e26aeffa5fbe518bdcc
gandazhi <18228438700@163.com>
2017-06-26 22:43:02 +0800

    配置好登录界面

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,20 @@
+# _*_ coding:utf-8 _*_
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('username', '')
+        pass_word = request.POST.get('password', '')
+        user = authenticate(username=user_name, password=pass_word)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'login.html', {})
+    elif request.method == 'GET':
+        return render(request, 'login.html', {})
