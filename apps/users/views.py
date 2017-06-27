0339ef0342ee66a254c392c2b995cebc73d33d2a
gandazhi <18228438700@163.com>
2017-06-27 23:55:07 +0800

    自定义authenticate方法，实现使用邮箱登录帐号

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,22 @@
 # _*_ coding:utf-8 _*_
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+from .models import UserProfile
 
 # Create your views here.
+
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        try:
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
 
 
 def user_login(request):
