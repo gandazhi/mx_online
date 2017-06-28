131cfdbd61bb6fcc07fbdbe54e45a1374afb053d
gandazhi <18228438700@163.com>
2017-06-28 21:34:09 +0800

    使用基于class，配置好登录操作 在users/views中使用class继承django中的View，写get 和 post方法，等于request.method == 'POST'和 request.method == 'GET' 在urls中import新写的class，配置url时，使用class.as_view()

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,10 +3,28 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from .models import UserProfile
 
 # Create your views here.
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html', {})
+
+    def post(self, request):
+        user_name = request.POST.get('username', '')
+        pass_word = request.POST.get('password', '')
+        user = authenticate(username=user_name, password=pass_word)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'login.html', {'msg': '用户名或密码错误'})
+
 
 
 class CustomBackend(ModelBackend):
@@ -18,17 +36,3 @@ class CustomBackend(ModelBackend):
         except Exception as e:
             return None
 
-
-def user_login(request):
-    if request.method == 'POST':
-        user_name = request.POST.get('username', '')
-        pass_word = request.POST.get('password', '')
-        user = authenticate(username=user_name, password=pass_word)
-
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html')
-        else:
-            return render(request, 'login.html', {'msg': '用户名或密码错误'})
-    elif request.method == 'GET':
-        return render(request, 'login.html', {})
