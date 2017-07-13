d2572552f7c1cf2a119a5bda32dd7d1b88535f74
gandazhi <18228438700@163.com>
2017-07-13 21:59:00 +0800

    完成公开课的分页处理

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,6 +1,7 @@
 # _*_ coding:utf-8 _*_
 from django.shortcuts import render
 from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Course
 
@@ -13,5 +14,12 @@ class CouresListView(View):
         course = Course.objects.all()
         current_page = 'course'
         hot_course = course.order_by('fav_num')[:3]
-        return render(request, 'course-list.html', {'course': course, 'current_page': current_page,
-                                                    'hot_course': hot_course})
+
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        p = Paginator(course, 6, request=request)
+        course_list = p.page(page)
+        return render(request, 'course-list.html',
+                      {'current_page': current_page, 'hot_course': hot_course, 'course_list': course_list})
