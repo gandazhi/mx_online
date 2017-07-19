2181a305b645076fc032715fab01623e250b3910
gandazhi <18228438700@163.com>
2017-07-19 22:43:52 +0800

    配置好课程评论页 完成发表评论（使用ajax，传course_id和comments到后端数据接口，保存到数据库） 完成显示已经有的评论（通过course找到当前课程的评论，显示在页面上）

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,7 +1,7 @@
 # _*_coding:utf-8 _*_
 from django.conf.urls import url, include
 
-from courses.views import CoursesListView, CourseDetail, CourseInfoView
+from courses.views import CoursesListView, CourseDetail, CourseInfoView, CommentsView, AddCommentsView
 
 urlpatterns = [
 
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^list/$', CoursesListView.as_view(), name='course_list'),
     url(r'^detail/(?P<course_id>\d+)/$', CourseDetail.as_view(), name='course_detail'),
     url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_info'),
+    url(r'^comments/(?P<course_id>\d+)/$', CommentsView.as_view(), name='course_comments'),
+    # 添加课程评论
+    url(r'^add_comment/$', AddCommentsView.as_view(), name='add_comment'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,10 +2,11 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.db.models import Q
+from django.http import HttpResponse
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Course, CourseResource
-from operation.models import UserFavorite
+from operation.models import UserFavorite, CourseComments
 
 
 # Create your views here.
@@ -40,7 +41,6 @@ class CourseDetail(View):
     def get(self, request, course_id):
         current_page = 'course'
         course = Course.objects.get(id=int(course_id))
-        print course
         """课程记录点击数"""
         course.click_num += 1
         course.save()
@@ -57,7 +57,6 @@ class CourseDetail(View):
             relate_course = Course.objects.filter(tag=tag)[:1]
             for e in relate_course.all():  # 判断取出relate_course_id
                 relate_course_id = e.id
-                print relate_course
                 if relate_course_id == course.id:  # 判断relate_course.id是否与当前课程.id相同，如果相同就再重新取一个
                     relate_course = Course.objects.filter(Q(tag=tag), ~Q(id=course_id))
         else:
@@ -74,4 +73,36 @@ class CourseInfoView(View):
         course = Course.objects.get(id=int(course_id))
         lesson = course.lesson_set.all()
         all_resources = CourseResource.objects.filter(course=course)
-        return render(request, 'course-video.html', {'course': course, 'lesson': lesson, 'all_resources': all_resources})
+        return render(request, 'course-video.html',
+                      {'course': course, 'lesson': lesson, 'all_resources': all_resources})
+
+
+class CommentsView(View):
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        all_comments = CourseComments.objects.filter(course=course)
+        return render(request, 'course-comment.html',
+                      {'course': course, 'all_resources': all_resources, 'all_comments': all_comments})
+
+
+class AddCommentsView(View):
+    """用户添加课程评论"""
+
+    def post(self, request):
+        if not request.user.is_authenticated():
+            return HttpResponse('{"status": "fail", "msg": "用户未登录"}', content_type='application/json')
+
+        course_id = request.POST.get('course_id', 0)
+        comments = request.POST.get('comments', '')
+
+        if course_id > 0 and comments:
+            course_comments = CourseComments()
+            course = Course.objects.get(id=int(course_id))
+            course_comments.course = course
+            course_comments.comments = comments
+            course_comments.user = request.user
+            course_comments.save()
+            return HttpResponse('{"status": "success", "msg": "添加成功"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status": "fail", "msg": "添加失败"}', content_type='application/json')
